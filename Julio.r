2f9ac2eee7212c0a718fe1e4186b74a6ac62f27e
Julio de Melo <68486351+Julio-sourc@users.noreply.github.com>
2022-03-20 15:52:46 -0300

    Add files via upload

--- Julio.r
+++ Julio.r
@@ -1,20 +1,22 @@
+library(tidyverse)
+library(magrittr)
+
+install.packages('dplyr')
+library(dplyr)
+
 credi = read.csv('credit_data.csv')
 
 head(credi)
 
-head(credi)
-
-library(tidyverse)
-library(magrittr)
-credi$clientid - NULL
-credi %>% select(-clientid) %>% head()
+credi$i.clientid <- NULL
+#credi %>% select(-clientid) %>% head()
 
 #algumas estátisticas
 summary(credi)
 
 credi [credi$age < 0, ]# vazio retorna todas as colunas
 
-credi [credi$age < 0, 2:3]
+credi [credi$age < 0, 1:2]
 
 credi = credi[credi$age > 0, ]
 
@@ -30,20 +32,45 @@ credi = credi[credi$age > 0, ]
 #através da foruma da média na linha 11, mean(credi$age > 0, na.rm= TRUE)
 credi$age = ifelse(credi$age < 0, 40.92, credi$age)
 
-credi[is.na(credi$age)]
-base$age = ifelse(is.na(base$age),mean (credi$age, na.rm = TRUE), credi$age)
+credi[is.na(credi$age), ]
+credi$age = ifelse(is.na(credi$age), mean(credi$age, na.rm =TRUE), credi$age)
 # se a base for igual a nulo preencha a média, senão mantenha 
 
+head(credi)
 
-credi %>% 
-  janitor::clean_names() %>% 
-  group_by(incame) %>% 
-  summarise(
-    n = n(),
-    media_idade = mean(age),
-    mediana_idade = median(age),
-    media_renda = mean(income),
-    mediana_renda = median(income)
-  ) %>% 
-  arrange(-renda)
-```
+credi %>%
+filter(income=='age') %>%
+   group_by(age) %>%
+   summarise(mean(loan))
+
+#média de emprestimo por idade
+credi %>% group_by(age)%>%
+summarise(mean(loan))
+
+#escalonamento padronização
+credi[, 1:3] = scale(credi[, 1:3])
+
+library(ggplot2) # `require(ggplot2)` também serve 99.99% usam `library()`
+
+options(scipen = 999)
+
+ggplot(credi, aes(x=age, y=loan))
+
+ggplot(credi, aes(x=age, y=loan))+
+geom_point(aes(col=income, size=loan))
+
+ggplot(credi, aes(x=age, y=loan))+
+geom_point(aes(col=income, size=loan))+
+xlim(c(0, 1.1742218))+
+ylim(c(0, 1.6724960))
+
+ggplot(credi, aes(x=age, y=loan))+
+geom_point(aes(col=income, size=loan))+
+xlim(c(0, 1.1742218))+
+ylim(c(0, 1.6724960))+
+geom_smooth(method = loess, se=T)+
+
+
+min(credi)
+
+
